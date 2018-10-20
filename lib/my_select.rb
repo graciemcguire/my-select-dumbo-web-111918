a75780bdb875d5931340ce94de66ae4f0e341adb
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-20 18:55:47 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -10,16 +10,3 @@ def my_select(array)
   new_array
 end
 
-
-def my_select(array) 
-  i =0 
-  select = [] 
-  while i < array.length 
-    if (yield(array[i]))
-      # select.push(array[i])OR 
-      select << array[i] 
-    end 
-    i += 1 
-  end 
-  select 
-end 
