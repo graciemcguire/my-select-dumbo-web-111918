6999e877d9d92f8f9c6aa06801294ec5643960b4
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-20 18:55:39 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,24 @@ def my_select(array)
  i = 0 
  new_array = []
  while i < array.length
- new_array << yield(array[i])
+  if(yield(array[i]))
+  new_array << array[i]
+  end
     i += 1
   end
   new_array
 end
+
+
+def my_select(array) 
+  i =0 
+  select = [] 
+  while i < array.length 
+    if (yield(array[i]))
+      # select.push(array[i])OR 
+      select << array[i] 
+    end 
+    i += 1 
+  end 
+  select 
+end 
