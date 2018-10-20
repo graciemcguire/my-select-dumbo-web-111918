1679e4cad3fadd25966fbd8bc998d67e8dd0ee17
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-20 18:48:08 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
-def my_select(collection)
- # your code here!
+def my_select(selection)
+ i = 0 
+ new_select = []
+ while i < selection.length
+end
+
+
+def my_collect(collection)
+   i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
 end
